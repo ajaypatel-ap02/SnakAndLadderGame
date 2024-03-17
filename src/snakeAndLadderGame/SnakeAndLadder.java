e9815b3c6fb15a92c1286f4a8b7e9aa09890d856
ajayaptel-ap02 <26patilajay@gmail.com>
2024-03-17 07:36:00 +0530

    [MODIFIED] : Ajay - player starting position

--- src/snakeAndLadderGame/SnakeAndLadder.java
+++ src/snakeAndLadderGame/SnakeAndLadder.java
@@ -1,11 +1,19 @@
 package snakeAndLadderGame;
 
 public class SnakeAndLadder {
+	private int player_position = 0 ;
+	
+	public void showPosition()
+	{
+		System.out.println("player Position :" + player_position );
+	}
 
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome To Snack and Ladder Simulator");
-				
+		
+		SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+		snakeAndLadder.showPosition();
 	}
 
 }
