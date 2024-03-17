86d0fc85b1db0d22d54d05eabdd9b0280929480f
ajayaptel-ap02 <26patilajay@gmail.com>
2024-03-17 07:43:40 +0530

    [MODIFIED] : Ajay - player dieing the roll

--- src/snakeAndLadderGame/SnakeAndLadder.java
+++ src/snakeAndLadderGame/SnakeAndLadder.java
@@ -1,19 +1,31 @@
 package snakeAndLadderGame;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
-	private int player_position = 0 ;
+	
+	private int playerPosition = 0 ;
 	
 	public void showPosition()
 	{
-		System.out.println("player Position :" + player_position );
+		System.out.println("player Position :" + playerPosition );
 	}
-
+	
+	public void playGame()
+	{
+		Random random = new Random();
+		int die = random.nextInt(6)+1;
+		
+		System.out.println("Die No :" + die);
+	}
+	
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome To Snack and Ladder Simulator");
 		
 		SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
 		snakeAndLadder.showPosition();
+		snakeAndLadder.playGame();
 	}
 
 }
