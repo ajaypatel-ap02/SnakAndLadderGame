6bc49329fe4f14b90030dcd68934f262f7448fb9
ajayaptel-ap02 <26patilajay@gmail.com>
2024-03-17 07:31:57 +0530

    [ADDED] : Ajay - Snake and ladder game simulator

---
+++ src/snakeAndLadderGame/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package snakeAndLadderGame;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome To Snack and Ladder Simulator");
+				
+	}
+
+}
